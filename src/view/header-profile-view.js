033b78a6e9818bbdd08a61e3119b76edfbaf8567
AOhapkin <alexeyyago@gmail.com>
2023-06-29 11:11:21 +0300

    SortView

--- src/view/header-profile-view.js
+++ src/view/header-profile-view.js
@@ -1,34 +1,33 @@
-/* eslint-disable indent */
-import { createElement } from '../render';
+import { createElement } from '../render.js';
 
 const createHeaderProfileTemplate = () =>
-	`
-		<section class="header__profile profile">
-			<p class="profile__rating">Movie Buff</p>
-			<img 
-				class="profile__avatar"
-				src="images/bitmap@2x.png"
-				alt="Avatar"
-				width="35"
-				height="35"
-			>
-		</section>
+  `
+    <section class="header__profile profile">
+      <p class="profile__rating">Movie Buff</p>
+      <img 
+        class="profile__avatar"
+        src="images/bitmap@2x.png"
+        alt="Avatar"
+        width="35"
+        height="35"
+      >
+    </section>
   `;
 
 export default class HeaderProfileView {
-	getTemplate() {
-		return createHeaderProfileTemplate();
-	}
+  getTemplate() {
+    return createHeaderProfileTemplate();
+  }
 
-	getElement() {
-		if (!this.element) {
-			this.element = createElement(this.getTemplate());
-		}
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
 
-		return this.element;
-	}
+    return this.element;
+  }
 
-	removeElement() {
-		this.element = null;
-	}
+  removeElement() {
+    this.element = null;
+  }
 }

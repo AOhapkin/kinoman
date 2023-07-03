cb7bfc358ac65ec99638d95fd34bec55579c9159
AOhapkin <alexeyyago@gmail.com>
2023-07-03 18:38:53 +0300

    Add consts for mock data

---
+++ src/mock/const.js
@@ -0,0 +1,122 @@
+const NAME_COUNT = 2;
+
+const MAX_COMMENTS_ON_FILM = 5;
+
+const GenreCount = {
+  MIN: 1,
+  MAX: 3
+};
+
+const Rating = {
+  MIN: 0,
+  MAX: 10
+};
+
+const AgeRating = {
+  MIN: 0,
+  MAX: 18
+};
+
+const Runtime = {
+  MIN: 60,
+  MAX: 180
+};
+
+const YearsDuration = {
+  MIN: 5,
+  MAX: 10
+};
+
+const DaysDuration = {
+  MIN: 0,
+  MAX: 7
+};
+
+const titles = [
+  'Country On Him',
+  'Raiders With The Carpet',
+  'Guest Who Sold The Darkness',
+  'Illegal Hayatlar',
+  'Extraction',
+  'Un amour impossible',
+  'Was man von hier aus sehen kann',
+  'The Midnight Mirage',
+  'Eternal Echoes',
+  'Whispers in the Wind',
+  'Shadow Edge',
+  'Crimson Sky',
+  'Serendipity Secrets',
+  'Beyond the Horizon',
+  'Enigmatic Enchantment',
+  'The Forgotten Star',
+  'Chasing Moonbeams'
+];
+
+const posters = [
+  'images/posters/made-for-each-other.png',
+  'images/posters/popeye-meets-sinbad.png',
+  'images/posters/sagebrush-trail.jpg',
+  'images/posters/santa-claus-conquers-the-martians.jpg',
+  'images/posters/the-dance-of-life.jpg',
+  'images/posters/the-great-flamarion.jpg',
+  'images/posters/the-man-with-the-golden-arm.jpg',
+];
+
+const genres = [
+  'Animation',
+  'Action',
+  'Adventure',
+  'Comedy',
+  'Family',
+  'Horror',
+  'Thriller',
+];
+
+const names = [
+  'Alexander',
+  'Catherine',
+  'Michael',
+  'Olga',
+  'Ivan',
+  'Anna',
+  'Dmitry',
+  'Tatiana',
+  'Andrew',
+  'Natalia',
+];
+
+const surnames = [
+  'Smith',
+  'Johnson',
+  'Williams',
+  'Brown',
+  'Jones',
+  'Miller',
+  'Davis',
+  'Garcia',
+  'Rodriguez',
+  'Martinez',
+];
+
+const emotions = [
+  'smile',
+  'sleeping',
+  'puke',
+  'angry'
+];
+
+const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra.';
+
+const comment = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget.';
+
+const countries = [
+  'USA',
+  'Russia',
+  'Germany',
+  'Finland',
+  'France',
+  'Spain',
+  'Italy',
+  'China',
+  'Japan'
+];

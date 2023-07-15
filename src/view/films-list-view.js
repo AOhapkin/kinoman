d553c7f1e71f559c0336fe9a9013e5abbf1603f4
AOhapkin <alexeyyago@gmail.com>
2023-07-15 16:33:58 +0300

    refactor views

--- src/view/films-list-view.js
+++ src/view/films-list-view.js
@@ -1,4 +1,4 @@
-import { createElement } from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 
 const createFilmsListViewTemplate = () =>
   `
@@ -7,22 +7,8 @@ const createFilmsListViewTemplate = () =>
     </div>
 `;
 
-export default class FilmsListView {
-  #element = null;
-
+export default class FilmsListView extends AbstractView {
   get template() {
     return createFilmsListViewTemplate();
-  }
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
-  removeElement() {
-    this.#element = null;
   }
 }

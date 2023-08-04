7e41fda72a07986c4a3394306fe788f1bc5890b5
AOhapkin <alexeyyago@gmail.com>
2023-08-04 22:19:08 +0500

    update comments model with Observable

--- src/model/comments-model.js
+++ src/model/comments-model.js
@@ -1,25 +1,49 @@
+import Observable from '../framework/observable';
 import { generateComments } from '../mock/comment.js';
 
-export default class CommentsModel {
-  filmsModel = null;
-  allComments = [];
-  comments = [];
+export default class CommentsModel extends Observable {
+  #filmsModel = null;
+  #allComments = [];
+  #comments = [];
 
   constructor(filmsModel) {
-    this.filmsModel = filmsModel;
-    this.generateAllComments();
+    super();
+    this.#filmsModel = filmsModel;
+    this.#generateAllComments();
   }
 
-  generateAllComments() {
-    this.allComments = generateComments(this.filmsModel.get());
+  #generateAllComments() {
+    this.#allComments = generateComments(this.#filmsModel.get());
   }
 
   get = (film) => {
-    this.comments = film.comments.map((commentId) =>
-      this.allComments.find((comment) =>
+    this.#comments = film.comments.map((commentId) =>
+      this.#allComments.find((comment) =>
         comment.id === commentId)
     );
 
-    return this.comments;
+    return this.#comments;
+  };
+
+  add = (updateType, update) => {
+    this.#allComments.push(update);
+    this._notify(updateType, update);
+  };
+
+  delete = (updateType, update) => {
+    const index = this.#allComments.findIndex(
+      (comment) => comment.id === update.id
+    );
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting comment');
+    }
+
+    this.#allComments = [
+      ...this.#allComments.slice(0, index),
+      ...this.#allComments.slice(index + 1),
+    ];
+
+    this._notify(updateType);
   };
 }
